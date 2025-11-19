d9ed53e24256dfd6726954f37f3a888882c43d59
elazar matan <elazarmata8@gmail.com>
2025-11-19 11:46:36 +0200

    add a component audiosquare

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import AudioSquare from "./components/AudioSquare"
 
 function App() {
   return (
-    <div>App</div>
+    <div className="square">
+      <AudioSquare pathAudio="http://localhost:9000/try/1763539580699-B1vH.wav"/>
+    </div>
   )
 }
 
