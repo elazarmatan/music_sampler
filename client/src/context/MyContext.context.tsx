f048a891c66254bdbf6fb126403195d3c07c2710
elazar matan <elazarmata8@gmail.com>
2025-11-23 15:13:04 +0200

    add audio URLs and playback state to context provider

--- client/src/context/MyContext.context.tsx
+++ client/src/context/MyContext.context.tsx
@@ -1,10 +1,12 @@
-import React, { createContext , useContext, useEffect, useState } from 'react'
+import React, { createContext , useContext, useEffect, useRef, useState } from 'react'
 
 type contextType = {
     addColumn:number
     setAddcolumn:React.Dispatch<React.SetStateAction<number>>
     gridState:boolean[][]
     setGridState:React.Dispatch<React.SetStateAction<boolean[][]>>
+    urls:string[]
+    isPlaying: React.RefObject<boolean>
 }
 
 interface providerProps{
@@ -15,7 +17,16 @@ export const context = createContext<contextType | null>(null)
 
 export function MyContext(props:providerProps) {
   const [addColumn,setAddcolumn] = useState<number>(5)
-  
+  const isPlaying = useRef(false)
+  const urls = [
+    "http://localhost:9000/piano/piano/A1vH.wav",
+    "http://localhost:9000/piano/piano/A2vL.wav",
+    "http://localhost:9000/piano/piano/A3vH.wav",
+    "http://localhost:9000/piano/piano/A4vH.wav",
+    "http://localhost:9000/piano/piano/A5vH.wav",
+    "http://localhost:9000/piano/piano/A6vH.wav",
+    "http://localhost:9000/piano/piano/A7vH.wav",
+  ];
   const [gridState,setGridState] = useState<boolean[][]>(
     Array.from({length:addColumn},() => 
         Array.from({length:7},() => true)
@@ -32,7 +43,7 @@ export function MyContext(props:providerProps) {
       return prev;
     });
   }, [addColumn]);
-  return <context.Provider value={{addColumn,setAddcolumn,gridState,setGridState}}>{props.children}</context.Provider>
+  return <context.Provider value={{addColumn,setAddcolumn,gridState,setGridState,urls,isPlaying}}>{props.children}</context.Provider>
 }
 
 export function useMyContext(){
