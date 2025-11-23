63739c2191b611a117b49a06e3b50d4170fd94dd
elazar matan <elazarmata8@gmail.com>
2025-11-23 15:12:36 +0200

    update AudioSquare component to accept colorAfter prop for dynamic background color

--- client/src/components/AudioSquare.tsx
+++ client/src/components/AudioSquare.tsx
@@ -1,24 +1,24 @@
-import { useState } from "react"
 import * as Tone from 'tone'
 
 interface props{
     pathAudio:string
     active:boolean
     onToggle:() => void
+    colorAfter:string
 }
 
-function AudioSquare({pathAudio , active , onToggle}:props) {
+function AudioSquare({pathAudio , active , onToggle ,colorAfter}:props) {
   const play = async() => {
         await Tone.start()
         const audio = new Tone.Player(pathAudio).toDestination()
         await audio.load(pathAudio)
-         if(active){
-            audio.start()
+        if(active){
+          audio.start()
         }
         onToggle()
     }
   return (
-    <div onClick={play} className="square" style={{background:active ? "aliceblue":"blue"}}></div>
+    <div onClick={play} className="square" style={{background:active ? "aliceblue":colorAfter}}></div>
   )
 }
 
