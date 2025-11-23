62970248e7676fa65a090ca236e74b5395486dac
elazar matan <elazarmata8@gmail.com>
2025-11-23 10:11:32 +0200

    create NavBar component to include AddColumn and RemoveColumn buttons

---
+++ client/src/components/layout/NavBar.tsx
@@ -0,0 +1,13 @@
+import AddColumn from "../buttons/AddColumn";
+import RemoveColumn from "../buttons/RemoveColumn";
+
+function NavBar() {
+  return (
+    <footer>
+        <AddColumn/>
+        <RemoveColumn/>
+    </footer>
+  )
+}
+
+export default NavBar
