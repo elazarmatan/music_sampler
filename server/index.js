f722e40471653b345dab5cae8fcf7f3012c44df2
elazar matan <elazarmata8@gmail.com>
2025-11-26 11:15:58 +0200

    create basic server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import "dotenv/config";
+import cors from 'cors'
+import router from './routes/get.js';
+
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.static("public"))
+
+app.use('/',router)
+
+app.listen(process.env.PORT,() => console.log(`server listening on port ${process.env.PORT}`))
