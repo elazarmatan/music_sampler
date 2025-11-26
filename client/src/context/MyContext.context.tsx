1bd79044edf55ff5aa3740b72fcfcc3f7609841e
elazar matan <elazarmata8@gmail.com>
2025-11-26 11:14:55 +0200

    feat: Fetch and set audio URLs from server.
    This commit fetches piano audio URLs from the server on component mount.
    It also updates the context to include `setUrls` to manage the URLs state.
    The default speed has also been updated to 500ms.

--- client/src/context/MyContext.context.tsx
+++ client/src/context/MyContext.context.tsx
@@ -7,6 +7,7 @@ type contextType = {
     gridState:boolean[][]
     setGridState:React.Dispatch<React.SetStateAction<boolean[][]>>
     urls:string[]
+    setUrls:React.Dispatch<React.SetStateAction<string[]>>
     isPlaying: React.RefObject<boolean>
     controllSpeed:number
     setcontrollSpeed:React.Dispatch<React.SetStateAction<number>>
@@ -24,19 +25,29 @@ export const context = createContext<contextType | null>(null)
 export function MyContext(props:providerProps) {
   const [addColumn,setAddcolumn] = useState<number>(5)
   const isPlaying = useRef<boolean>(false)
-  const [controllSpeed, setcontrollSpeed] = useState(1000);
+  const [controllSpeed, setcontrollSpeed] = useState(500);
   const [column,setColumn] = useState(-1)
   const gain = useRef(new Tone.Gain(1).toDestination())
-  const urls = [
-    "http://localhost:9000/piano/piano/A1vH.wav",
-    "http://localhost:9000/piano/piano/A2vL.wav",
-    "http://localhost:9000/piano/piano/A3vH.wav",
-    "http://localhost:9000/piano/piano/A4vH.wav",
-    "http://localhost:9000/piano/piano/A5vH.wav",
-    "http://localhost:9000/piano/piano/A6vH.wav",
-    "http://localhost:9000/piano/piano/A7vH.wav",
-  ];
+  const [urls,setUrls] = useState([''])
+  
 
+  useEffect(() => {
+    const channel = async() =>{
+      const piano = await fetch("http://localhost:3005/channel")
+      const finishdata = await piano.json()
+      console.log(finishdata.piano)
+      setUrls(finishdata.piano)
+      // setGridState(
+      //   Array.from({length:addColumn},() => 
+      //   Array.from({length:urls.length},() => true)
+      //   )
+      // )
+      
+    console.log(addColumn,urls.length)
+    }
+    channel()
+  },[])
+  
   const [gridState,setGridState] = useState<boolean[][]>(
     Array.from({length:addColumn},() => 
         Array.from({length:7},() => true)
@@ -54,11 +65,11 @@ export function MyContext(props:providerProps) {
       return prev;
     });
   }, [addColumn]);
-  return <context.Provider value={{addColumn,setAddcolumn,gridState,setGridState,urls,isPlaying,controllSpeed,setcontrollSpeed,column,setColumn,gain}}>{props.children}</context.Provider>
+  return <context.Provider value={{addColumn,setAddcolumn,gridState,setGridState,urls,isPlaying,controllSpeed,setcontrollSpeed,column,setColumn,gain,setUrls}}>{props.children}</context.Provider>
 }
 
 export function useMyContext(){
     const myContext = useContext(context)
-    if (!myContext) throw new Error("useTimer must be used within a TimerProvider");
+    if (!myContext) throw new Error("error");
     return myContext
 }
