4fe2c39033abfc317dae01a9a264144c49a3784c
elazar matan <elazarmata8@gmail.com>
2025-11-27 16:25:27 +0200

    ``` feat: Remove unused state import from App.tsx.
    This commit removes an unused import statement.
    ```

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react"
 import GridMusic from "./components/layout/GridMusic"
 import Logo from "./components/layout/Logo"
 import NavBar from "./components/layout/NavBar"
-import Slogen from "./components/layout/Slogen"
 import { MyContext } from "./context/MyContext.context"
 
 function App() {
