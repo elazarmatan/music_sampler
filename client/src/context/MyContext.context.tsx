c18c32eab59d3a544d995540cc3771de98ac0636
elazar matan <elazarmata8@gmail.com>
2025-11-27 16:27:31 +0200

    ``` feat: Add error handling and active state to context.
    This commit introduces error handling to manage API fetch failures.
    Also, adds an active state for future UI interactions.
    ```

--- client/src/context/MyContext.context.tsx
+++ client/src/context/MyContext.context.tsx
@@ -1,58 +1,75 @@
-import React, { createContext , useContext, useEffect, useRef, useState } from 'react'
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import * as Tone from "tone";
 
 type contextType = {
-    addColumn:number
-    setAddcolumn:React.Dispatch<React.SetStateAction<number>>
-    gridState:boolean[][]
-    setGridState:React.Dispatch<React.SetStateAction<boolean[][]>>
-    urls:string[]
-    setUrls:React.Dispatch<React.SetStateAction<string[]>>
-    isPlaying: React.RefObject<boolean>
-    controllSpeed:number
-    setcontrollSpeed:React.Dispatch<React.SetStateAction<number>>
-    column:number
-    setColumn:React.Dispatch<React.SetStateAction<number>>
-    gain:React.RefObject<Tone.Gain<"gain">>
+  addColumn: number;
+  setAddcolumn: React.Dispatch<React.SetStateAction<number>>;
+  gridState: boolean[][];
+  setGridState: React.Dispatch<React.SetStateAction<boolean[][]>>;
+  urls: string[];
+  setUrls: React.Dispatch<React.SetStateAction<string[]>>;
+  isPlaying: React.RefObject<boolean>;
+  controllSpeed: number;
+  setcontrollSpeed: React.Dispatch<React.SetStateAction<number>>;
+  column: number;
+  setColumn: React.Dispatch<React.SetStateAction<number>>;
+  gain: React.RefObject<Tone.Gain<"gain">>;
+  error:boolean
+  setError:React.Dispatch<React.SetStateAction<boolean>>
+  active:boolean
+  setActive:React.Dispatch<React.SetStateAction<boolean>>
+};
+
+interface providerProps {
+  children: React.ReactNode;
 }
 
-interface providerProps{
-    children:React.ReactNode
-}
+export const context = createContext<contextType | null>(null);
 
-export const context = createContext<contextType | null>(null)
-
-export function MyContext(props:providerProps) {
-  const [addColumn,setAddcolumn] = useState<number>(5)
-  const isPlaying = useRef<boolean>(false)
+export function MyContext(props: providerProps) {
+  const [addColumn, setAddcolumn] = useState<number>(5);
+  const isPlaying = useRef<boolean>(false);
   const [controllSpeed, setcontrollSpeed] = useState(500);
-  const [column,setColumn] = useState(-1)
-  const gain = useRef(new Tone.Gain(1).toDestination())
-  const [urls,setUrls] = useState([''])
-  
+  const [column, setColumn] = useState(-1);
+  const gain = useRef(new Tone.Gain(1).toDestination());
+  const [urls, setUrls] = useState([""]);
+  const [error,setError] = useState(false)
+  const [active, setActive] = useState(false);
 
   useEffect(() => {
-    const channel = async() =>{
-      const piano = await fetch("http://localhost:3005/channel")
-      const finishdata = await piano.json()
-      console.log(finishdata.piano)
-      setUrls(finishdata.piano)
-      // setGridState(
-      //   Array.from({length:addColumn},() => 
-      //   Array.from({length:urls.length},() => true)
-      //   )
-      // )
-      
-    console.log(addColumn,urls.length)
-    }
-    channel()
-  },[])
-  
-  const [gridState,setGridState] = useState<boolean[][]>(
-    Array.from({length:addColumn},() => 
-        Array.from({length:7},() => true)
+    const channel = async () => {
+      try {
+        const piano = await fetch("http://localhost:3005/channel/piano");
+        if(piano.ok){
+          const finishdata = await piano.json();
+          setUrls(finishdata.piano);
+          setGridState(
+          Array.from({length:addColumn},() =>
+          Array.from({length:finishdata.piano.length},() => true)
+          )
+        )
+        }
+        else{
+          setError(true)
+        }
+      } catch (error) {
+        setError(true)
+      }
+    };
+    channel();
+  }, []);
+
+  const [gridState, setGridState] = useState<boolean[][]>(
+    Array.from({ length: addColumn }, () =>
+      Array.from({ length: urls.length }, () => true)
     )
-  )
+  );
 
   useEffect(() => {
     setGridState((prev) => {
@@ -65,11 +82,34 @@ export function MyContext(props:providerProps) {
       return prev;
     });
   }, [addColumn]);
-  return <context.Provider value={{addColumn,setAddcolumn,gridState,setGridState,urls,isPlaying,controllSpeed,setcontrollSpeed,column,setColumn,gain,setUrls}}>{props.children}</context.Provider>
+  return (
+    <context.Provider
+      value={{
+        addColumn,
+        setAddcolumn,
+        gridState,
+        setGridState,
+        urls,
+        isPlaying,
+        controllSpeed,
+        setcontrollSpeed,
+        column,
+        setColumn,
+        gain,
+        setUrls,
+        error,
+        setError,
+        active,
+        setActive
+      }}
+    >
+      {props.children}
+    </context.Provider>
+  );
 }
 
-export function useMyContext(){
-    const myContext = useContext(context)
-    if (!myContext) throw new Error("error");
-    return myContext
+export function useMyContext() {
+  const myContext = useContext(context);
+  if (!myContext) throw new Error("error");
+  return myContext;
 }
