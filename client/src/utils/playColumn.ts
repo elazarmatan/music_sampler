9775cc1ff239b0832ef67dc00e59d38be74bc7f6
elazar matan <elazarmata8@gmail.com>
2025-11-27 16:27:38 +0200

    ``` feat: Add isPlaying ref to playSpecificColumn function.
    This allows stopping playback mid-column.
    
    feat: Early return if isPlaying is false.
    
    Stops audio playback.
    
    feat: Break loop if isPlaying is false.
    
    Prevents further audio triggers.
    
    feat: Set timeout before checking isPlaying.
    
    Ensures current audio plays before stopping.
    ```

--- client/src/utils/playColumn.ts
+++ client/src/utils/playColumn.ts
@@ -7,6 +7,8 @@ interface params {
   setColumn: React.Dispatch<React.SetStateAction<number>>;
   controllSpeed:number
   gain:React.RefObject<Tone.Gain<"gain">>
+    isPlaying: React.RefObject<boolean>;
+  
 }
 
 export default async function playSpecificColumn({
@@ -15,18 +17,22 @@ export default async function playSpecificColumn({
   setColumn,
   urls,
   controllSpeed,
-  gain
+  gain,
+  isPlaying
 }: params) {
   if(!gridState[column].length) return
-  console.log("length",gridState[column].length)
+  // console.log("length",gridState[column].length)
   for (let row = 0; row < gridState[column].length; row ++) {
+    if(!isPlaying.current) break
     if (!gridState[column][row]) {
       const audio = new Tone.Player(urls[row]).connect(gain.current)
       await audio.load(urls[row]);
+      console.log(column,controllSpeed / 1000);
       audio.start();
     }
   }
   await new Promise((resulve) => setTimeout(resulve, controllSpeed));
+  if(!isPlaying.current)return
   if(column === gridState.length -1){
     setColumn(0)
   }
