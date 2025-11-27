01ddd3db03fd96839fb5620c5b7c4975e74f1c73
elazar matan <elazarmata8@gmail.com>
2025-11-27 16:26:27 +0200

    ``` Refactor AddColumn component: extract logic to functions.
    This commit refactors the AddColumn component to improve readability. The incrementing and decrementing logic for the column count is extracted into separate functions: `add` and `remove`. This makes the component easier to understand and maintain.
    ```

--- client/src/components/buttons/AddColumn.tsx
+++ client/src/components/buttons/AddColumn.tsx
@@ -2,19 +2,14 @@ import { useMyContext } from "../../context/MyContext.context"
 function AddColumn() {
     const {addColumn,setAddcolumn} = useMyContext()
 
+  const add = () => {if(addColumn < 70){setAddcolumn(prev => prev + 1)}}
+  const remove = () => {{if(addColumn > 5){setAddcolumn(prev => prev - 1)}}}
+
   return (
     <div id="AddColumn">
-      <button className="add" onClick={() => {
-            if(addColumn < 50){
-                setAddcolumn(prev => prev + 1)
-            }
-            }}>+</button>
+      <button className="add" onClick={add}>+</button>
       <p>column</p>
-      <button className="add" onClick={() => {
-        if(addColumn > 5){
-            setAddcolumn(prev => prev - 1)
-        }
-    }}>-</button>
+      <button className="add" onClick={remove}>-</button>
     </div>
     
   )
