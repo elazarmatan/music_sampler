853048d6c0be574a5118980ddf970da00a0a9116
elazar matan <elazarmata8@gmail.com>
2025-12-01 14:44:15 +0200

    Okay, here are some short, clear commit descriptions for the provided diff:
    *   **feat: Add darbuka channel.**
    *   **feat: Update channels to include darbuka.**
    *   **feat: Reset URLs on channel change.**

--- client/src/utils/handles/channels.ts
+++ client/src/utils/handles/channels.ts
@@ -2,15 +2,23 @@ interface params{
     namechannel:string
     setnamechannel:React.Dispatch<React.SetStateAction<string>>
     setchannel:React.Dispatch<React.SetStateAction<string>>
+    setUrls:React.Dispatch<React.SetStateAction<string[]>>
 }
 
-export default function channels({namechannel,setchannel,setnamechannel}:params){
+export default function channels({namechannel,setchannel,setnamechannel,setUrls}:params){
     switch (namechannel) {
         case "piano":
+          setUrls([''])
           setnamechannel("guitar")
           setchannel('🎸')
           break;
         case "guitar":
+          setUrls([''])
+          setnamechannel("darbuka")
+          setchannel('🪘')
+        break
+        case "darbuka":
+          setUrls([''])
           setnamechannel("piano")
           setchannel('🎹')
         break
