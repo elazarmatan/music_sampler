bb6a500e685c3280d42a5fe4a08f3f6527f47295
elazar matan <elazarmata8@gmail.com>
2025-12-01 14:43:39 +0200

    ``` feat: Reset grid on restart.
    Resets grid state, speed, column, and volume on restart.
    ```

--- client/src/components/buttons/Restart.tsx
+++ client/src/components/buttons/Restart.tsx
@@ -1,9 +1,9 @@
 import { useMyContext } from "../../context/MyContext.context";
 
 function Restart() {
-  const {setGridState,setAddcolumn,addColumn,setcontrollSpeed,setColumn,gain,} = useMyContext();
+  const {setGridState,setAddcolumn,addColumn,setcontrollSpeed,setColumn,gain,urls,setshowVolume} = useMyContext();
 
-  const restart = () => {setAddcolumn(5);setGridState(Array.from({ length: addColumn }, () =>Array.from({ length: 7 }, () => true)));setcontrollSpeed(500);setColumn(-1);gain.current.gain.value = 0.5}
+  const restart = () => {setAddcolumn(5);setGridState(Array.from({ length: addColumn }, () =>Array.from({ length: urls.length }, () => true)));setcontrollSpeed(500);setColumn(-1);gain.current.gain.value = 0.5,setshowVolume(0.5)}
 
   return (
     <button
