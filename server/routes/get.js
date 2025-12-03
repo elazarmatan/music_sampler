de2e92d1516921bfb32fd2aae567a70438b1f065
elazar matan <elazarmata8@gmail.com>
2025-12-03 15:36:54 +0200

    ``` Rename "piano" key to "music" in the JSON response.
    The key "piano" was not descriptive of the returned URLs. Renamed to "music".
    ```

--- server/routes/get.js
+++ server/routes/get.js
@@ -9,7 +9,7 @@ router.get('/channel/:channame',async(req,res) => {
   try {
     const urls = await getSignedUrlsFromFolder(req.params.channame)
     await new Promise(resolve => (setTimeout(resolve,2000)))
-    res.json({"piano":urls})
+    res.json({"music":urls})
   } catch (error) {
     res.status(500).json({"msg":error})
   }
