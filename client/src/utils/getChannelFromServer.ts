2c0c148bbc589a7936bfe8b04196089b97c1202f
elazar matan <elazarmata8@gmail.com>
2025-12-03 15:36:36 +0200

    ``` feat: Fetch and cache channel data from server.
    Fetches channel data, caches it locally, and updates state.
    ```

---
+++ client/src/utils/getChannelFromServer.ts
@@ -0,0 +1,48 @@
+import getChannellocal from "./getChanFromLocal";
+
+interface params{
+    setUrls: React.Dispatch<React.SetStateAction<string[]>>
+    setGridState: React.Dispatch<React.SetStateAction<boolean[][]>>;
+    setError:React.Dispatch<React.SetStateAction<boolean>>
+    addColumn: number;
+    namechannel:string
+}
+export default async function getChannel({setError,setGridState,setUrls,addColumn,namechannel}:params){
+  const time = getChannellocal(namechannel+"time")
+  const now = Date.now()
+  const sixHours = 6 * 60 * 60 * 1000;
+  if(now - time >= sixHours){
+    localStorage.removeItem(namechannel)
+    localStorage.removeItem(namechannel+"time")
+  }
+  const local = getChannellocal(namechannel)
+    if(!local){
+    try {
+        const music = await fetch(`http://localhost:3005/channel/${namechannel}`);
+        if(music.ok){
+          const finishdata = await music.json();
+          localStorage.setItem(namechannel,JSON.stringify(finishdata.music))
+          localStorage.setItem(namechannel+"time",JSON.stringify(Date.now()))
+          setUrls(finishdata.music);
+          setGridState(
+          Array.from({length:addColumn},() =>
+          Array.from({length:finishdata.music.length},() => true)
+            )
+          )
+        }
+        else{
+          setError(true)
+        }
+      } catch (error) {
+        setError(true)
+      }
+    }
+    else{
+      setUrls(local)
+      setGridState(
+          Array.from({length:addColumn},() =>
+          Array.from({length:local.length},() => true)
+            )
+          )
+    }
+}
