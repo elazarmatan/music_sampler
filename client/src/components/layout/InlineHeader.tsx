5cd1ad8b105195584bdba50b8c9be5ed8fd50bb9
elazar matan <elazarmata8@gmail.com>
2025-12-08 11:04:27 +0200

    change name from header to inlineheader

---
+++ client/src/components/layout/InlineHeader.tsx
@@ -0,0 +1,11 @@
+import '../../style/InlineHaeder.css'
+
+function InlineHaeder() {
+  return (
+    <header className='inlineHead'>
+      <img src="/logo.png" className="logo" />
+    </header>
+  )
+}
+
+export default InlineHaeder
