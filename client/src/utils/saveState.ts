195cfaf74eb0d25f39e761584394ab3ee7d7d9df
elazar matan <elazarmata8@gmail.com>
2025-12-08 14:47:43 +0200

    change set state

--- client/src/utils/saveState.ts
+++ client/src/utils/saveState.ts
@@ -1,18 +1,16 @@
-export default async function saveState(namesate:string,state:boolean[][],setloading:React.Dispatch<React.SetStateAction<boolean>>){
-    const response = await fetch(`http://localhost:3005/create/saveState/${namesate}`,{
+export default async function saveState(namesate:string,state:boolean[][],setAccesSave: React.Dispatch<React.SetStateAction<"pending" | "succes" | "failed" | "not">>){
+    setAccesSave('pending')
+    const response = await fetch(`http://localhost:3005/create/saveState/${namesate}${new Date().toLocaleString()}`,{
         method:"POST",
         headers:{
              "Content-Type": "application/json"
         },
         body:JSON.stringify(state)
     })
-    setloading(false)
     if(response.ok){
-        setloading(true)
-        return true
+        setAccesSave('succes')
     }
     else{
-        setloading(true)
-        return false
+        setAccesSave('failed')
     }
 }
