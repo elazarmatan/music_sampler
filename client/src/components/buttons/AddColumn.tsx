eeef9ad14e632576dc16abd3f7b5238030283c8b
elazar matan <elazarmata8@gmail.com>
2025-12-08 11:02:31 +0200

    change number column

--- client/src/components/buttons/AddColumn.tsx
+++ client/src/components/buttons/AddColumn.tsx
@@ -2,14 +2,14 @@ import { useMyContext } from "../../context/MyContext.context"
 function AddColumn() {
     const {addColumn,setAddcolumn} = useMyContext()
 
-  const add = () => {if(addColumn < 70){setAddcolumn(prev => prev + 1)}}
+  const add = () => {if(addColumn < 100){setAddcolumn(prev => prev + 1)}}
   const remove = () => {{if(addColumn > 5){setAddcolumn(prev => prev - 1)}}}
 
   return (
     <div id="AddColumn">
-      <button className="add" onClick={add}>+</button>
+      <button className="add button" onClick={add}>+</button>
       <p>column</p>
-      <button className="add" onClick={remove}>-</button>
+      <button className="add button" onClick={remove}>-</button>
     </div>
     
   )
