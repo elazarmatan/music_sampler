624059b93e47e7e77dcc096b098170c153e83327
elazar matan <elazarmata8@gmail.com>
2025-12-08 14:46:57 +0200

    add button save

--- client/src/components/layout/InlineHeader.tsx
+++ client/src/components/layout/InlineHeader.tsx
@@ -1,9 +1,11 @@
 import '../../style/InlineHaeder.css'
+import SaveState from '../buttons/SaveState'
 
 function InlineHaeder() {
   return (
     <header className='inlineHead'>
       <img src="/logo.png" className="logo" />
+      <SaveState/>
     </header>
   )
 }
