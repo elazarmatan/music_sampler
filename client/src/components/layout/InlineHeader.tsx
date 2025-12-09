df950e2924a77cee0b7f9dd7a57bc8f3ee2ac662
elazar matan <elazarmata8@gmail.com>
2025-12-09 15:59:38 +0200

    fix: add navigation button to redirect to music save page

--- client/src/components/layout/InlineHeader.tsx
+++ client/src/components/layout/InlineHeader.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from 'react-router'
 import '../../style/InlineHaeder.css'
 import SaveState from '../buttons/SaveState'
 
 function InlineHaeder() {
+  const musicsaveNav = useNavigate()
   return (
     <header className='inlineHead'>
+      <button onClick={() => {musicsaveNav('/music')}} className='button'>MUSIC SAVE</button>
       <img src="/logo.png" className="logo" />
       <SaveState/>
     </header>
