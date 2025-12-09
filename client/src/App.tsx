b7845358928cd0c09008203460af9d46ef32565b
elazar matan <elazarmata8@gmail.com>
2025-12-09 15:58:11 +0200

    add route named saved music

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router"
 import HomePage from "./pages/HomePage"
-
+import MusicSaves from "./pages/MusicSaves"
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      
       <Route path="/" element={<HomePage/>}/>
+      <Route path="/music" element={<MusicSaves/>}/>
     </Routes>
     </BrowserRouter>
   )
