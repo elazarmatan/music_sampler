25acb53e6325b82318a05b66eab04d1dd0c194e2
elazar matan <elazarmata8@gmail.com>
2025-12-09 15:58:54 +0200

    component that show details to music saved

---
+++ client/src/components/Music.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router"
+import { useMyContext } from "../context/MyContext.context"
+import parseFileKey from "../utils/parseFileKey"
+import '../style/musicsaved.css'
+
+interface props{
+    name:string
+    grid:boolean[][]
+    channel:string
+}
+
+function Music({name,grid,channel}:props) {
+    const {setGridState,setnamechannel,setAddcolumn} = useMyContext()
+    const homenav = useNavigate()
+    const fullName = parseFileKey(name)
+  return (
+    <div className="music" key={name}>
+        <h2>{fullName.fileName}</h2>
+        <p>created at: {fullName.createdAt}</p>
+        <p>channel: {channel}</p>
+        <button onClick={() => {
+            setAddcolumn(grid.length)
+            setnamechannel(channel)
+            setGridState(grid)
+            homenav('/')
+        }}className="button">play</button>
+    </div>
+  )
+}
+
+export default Music
