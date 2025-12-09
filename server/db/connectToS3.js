989e50e3ce65a77226031038a3d3cb1521c11b4d
elazar matan <elazarmata8@gmail.com>
2025-12-09 16:00:52 +0200

    feat: add downloadFiles function to retrieve and parse files from S3

--- server/db/connectToS3.js
+++ server/db/connectToS3.js
@@ -1,5 +1,6 @@
 import { S3Client,ListObjectsV2Command, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { json } from "express";
 
 
 const s3 = new S3Client({
@@ -43,4 +44,17 @@ export async function savestate(matrix,Key){
         Body:JSON.stringify(matrix),
         ContentType:'application/json'
     }))
+}
+
+export async function downloadFiles(folderName){
+    const keys = await listFiles(folderName)
+    const files = await Promise.all(
+        keys.map(async(key) => {
+            if(key.Size === 0) return
+            const res = await s3.send(new GetObjectCommand({Bucket:'music',Key:key.Key}))
+            const text = await res.Body.transformToString()
+            return {key,data:JSON.parse(text)}
+        })
+    )
+    return files
 }
