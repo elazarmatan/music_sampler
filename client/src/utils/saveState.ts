1ab3f7e7ef1754f5c2567cc01874a208a8941347
elazar matan <elazarmata8@gmail.com>
2025-12-09 16:00:39 +0200

    refactor: enhance saveState function by adding namechannel parameter and improving code readability

--- client/src/utils/saveState.ts
+++ client/src/utils/saveState.ts
@@ -1,16 +1,29 @@
-export default async function saveState(namesate:string,state:boolean[][],setAccesSave: React.Dispatch<React.SetStateAction<"pending" | "succes" | "failed" | "not">>){
-    setAccesSave('pending')
-    const response = await fetch(`http://localhost:3005/create/saveState/${namesate}${new Date().toLocaleString()}`,{
-        method:"POST",
-        headers:{
-             "Content-Type": "application/json"
+export default async function saveState(
+  namesate: string,
+  state: boolean[][],
+  setAccesSave: React.Dispatch<
+    React.SetStateAction<"pending" | "succes" | "failed" | "not">
+  >,
+  namechannel:string
+) {
+  setAccesSave("pending");
+  try {
+    const response = await fetch(
+      `http://localhost:3005/create/saveState/${namesate}${new Date().toLocaleString()}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify(state)
-    })
-    if(response.ok){
-        setAccesSave('succes')
+        body: JSON.stringify({"channel":namechannel,"matrix":state}),
+      }
+    );
+    if (response.ok) {
+      setAccesSave("succes");
+    } else {
+      setAccesSave("failed");
     }
-    else{
-        setAccesSave('failed')
-    }
+  } catch (error) {
+    setAccesSave("failed");
+  }
 }
