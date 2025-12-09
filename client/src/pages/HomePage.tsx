8b56f948359d0ef075f769009ff5d4b82cf7a5bc
elazar matan <elazarmata8@gmail.com>
2025-12-09 15:59:51 +0200

    fix: remove MyContext wrapper from HomePage component

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,17 +1,14 @@
-import { MyContext } from '../context/MyContext.context'
 import GridMusic from '../components/layout/GridMusic'
 import NavBar from '../components/layout/NavBar'
 import InlineHaeder from '../components/layout/InlineHeader'
 
 function HomePage() {
   return (
-        <MyContext>
             <div className='page'>
                 <InlineHaeder/>
                 <GridMusic/>
                 <NavBar/>
             </div>
-        </MyContext>
   )
 }
 
