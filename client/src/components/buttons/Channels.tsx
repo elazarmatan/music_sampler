fa3af0a53e6432488ef2f3157c6740f61fca1bae
elazar matan <elazarmata8@gmail.com>
2025-12-09 15:59:13 +0200

    create function logo channel

--- client/src/components/buttons/Channels.tsx
+++ client/src/components/buttons/Channels.tsx
@@ -2,10 +2,10 @@ import { useMyContext } from "../../context/MyContext.context"
 import channels from "../../utils/handles/channels"
 
 function Channels() {
-  const {setnamechannel,namechannel,channel,setchannel,setUrls} = useMyContext()
+  const {setnamechannel,namechannel,channel,setUrls} = useMyContext()
   return (
     <button className="channel button" onClick={() => {
-      channels({namechannel,setchannel,setnamechannel,setUrls})
+      channels({namechannel,setnamechannel,setUrls})
     }}>{channel}</button>
   )
 }
